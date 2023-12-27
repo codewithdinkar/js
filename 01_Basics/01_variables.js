79e261db1d54a199aad54d6f4bd4c2fcecba014c
codewithdinkar <154953696+codewithdinkar@users.noreply.github.com>
2023-12-27 13:39:45 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "codewithdinkar@gmail.com"
+let accountState;
+/*
+Always use Let as Variable
+*/
+var accountPassword= "12345"
+accountCity = "Saharsa"
+
+//accountId = 2
+// accountEmail = "abc@gmail.com"
+// accountPassword = "9999"
+// accountCity = "Bengaluru"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountPassword, accountState]);
